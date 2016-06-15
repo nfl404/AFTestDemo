26e5943894c6facee39e677a32e783937c069faf
niejia <niefuling@yonglibao.com>
2016-06-15 11:23:56 +0800

    init

---
+++ AFTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFTestDemo
+//
+//  Created by Mr nie on 16/6/15.
+//  Copyright © 2016年 程序猿:NiePlus   博客地址:http://nieplus.blog.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+inhibit_all_warnings!
+
+target "AFTestDemo" do
+pod 'AFNetworking', '~> 3.1.0'
+
+end
